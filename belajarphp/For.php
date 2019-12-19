a89a9d1cb5cdb923670764d91964d23146931857
ArRahmaan17 <54971774+ArRahmaan17@users.noreply.github.com>
2019-12-20 01:30:08 +0700

    i add comment

--- belajarphp/For.php
+++ belajarphp/For.php
@@ -1,6 +1,12 @@
 <?php 
 
-	for ($ulangi = 0; $ulangi < 10; $ulangi++) { 
+	// deklarasi variael 
+	for ($ulangi = 0; 
+	// pengecekan kondisi
+	$ulangi < 10; 
+	// kondisi nya mau diapakan
+	$ulangi++) { 
+		// dan apa yang ingin dilakukan
 		echo "<h2>ini adalah perulangan ke-$ulangi </h2>";
 	}
 
