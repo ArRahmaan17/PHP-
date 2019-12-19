7ae5d6d2ab05416b25132ca99f8917e27882fd4f
ArRahmaan17 <54971774+ArRahmaan17@users.noreply.github.com>
2019-12-20 01:28:51 +0700

    i add comment in this code

--- belajarphp/dowhile.php
+++ belajarphp/dowhile.php
@@ -1,7 +1,11 @@
 <?php 
+	// deklarasi variabel
 	$ulangi = 0;
+	//blok yang akan dikerjakan
 	do{
 		echo "<p>ini adalah perulangan ke-$ulangi</p>";
+		// kondisi nya mau diapakan
 		$ulangi++;
+		// pengecekan kondisi 
 	}while($ulangi < 10);
 ?>
