c8e8c0f50a722e824b481321a010055d32d330b4
ArRahmaan17 <54971774+ArRahmaan17@users.noreply.github.com>
2019-12-20 01:35:56 +0700

    i add comment

--- belajarphp/While.php
+++ belajarphp/While.php
@@ -1,9 +1,12 @@
 <?php 
-
+	// deklarasi variabel
 	$ulangi = 0;
 
+	// pengecekan kondisi 
 	while($ulangi < 10) {
+		// blok yang akan dieksekusi
 		echo "\t<p>Ini adalah perulangan ke-$ulangi</p> ";
+		// mau diapakan nilai yang berada di dalam variabel
 		$ulangi++;
 	}
 
