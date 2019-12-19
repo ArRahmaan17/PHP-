903143f02402b1cb46cbfc013cf80992a360cc3c
ArRahmaan17 <54971774+ArRahmaan17@users.noreply.github.com>
2019-12-20 01:33:33 +0700

    i add comment

--- belajarphp/ifelse.php
+++ belajarphp/ifelse.php
@@ -1,9 +1,12 @@
 <?php 
+	// deklarasi variabel
 	$nilai =70;
-
+	// pengecekan nilai benar
 	if ($nilai >= 60) {
+		// blok ini yang akan di jalankan apabila kondisi benar
 		echo "Nilai anda : $nilai. Selamat,Anda Telah Lulus";
 	}else{
+		// jika salah blok ini yang akan dijalankan
 		echo "Nilai Anda : $nilai. Sorry,Anda Tidak Lulus";
 	}
 ?>
